397ccaef0719d53804c705b08cfc2b22dc1c7239
Andrei Gushchin <neogan74@gmail.com>
2025-04-08 17:16:47 +0300

    dev: poc

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Test")
+	http.HandleFunc("/kv/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPut:
+			kvPut(w, r)
+		case http.MethodGet:
+			kvGet(w, r)
+		case http.MethodDelete:
+			kvDel(w, r)
+		default:
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+	log.Println("Server started at http://localhost:8888")
+	log.Fatal(http.ListenAndServe(":8888", nil))
 }
