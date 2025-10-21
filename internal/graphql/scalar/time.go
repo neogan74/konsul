5c4dffd7e157d717e3011e993c55479c6442fe41
Gushchin Andrei <neogan74@gmail.com>
2025-10-21 21:19:18 +0500

    chore: refactor Time scalar to implement GraphQL Marshaler/Unmarshaler
    - Replaced `MarshalTime` and `UnmarshalTime` with `Time` type implementing Marshaler and Unmarshaler interfaces.
    - Added helper methods `ToTime` and `FromTime` for conversions.
    - Improved RFC333

--- internal/graphql/scalar/time.go
+++ internal/graphql/scalar/time.go
@@ -8,17 +8,37 @@ import (
 	"github.com/99designs/gqlgen/graphql"
 )
 
-// MarshalTime marshals time.Time to RFC3339 string
-func MarshalTime(t time.Time) graphql.Marshaler {
-	return graphql.WriterFunc(func(w io.Writer) {
-		io.WriteString(w, fmt.Sprintf(`"%s"`, t.Format(time.RFC3339)))
-	})
+// Time is a custom scalar type for timestamps
+type Time time.Time
+
+// MarshalGQL implements the graphql.Marshaler interface
+func (t Time) MarshalGQL(w io.Writer) {
+	timestamp := time.Time(t).Format(time.RFC3339)
+	io.WriteString(w, fmt.Sprintf(`"%s"`, timestamp))
 }
 
-// UnmarshalTime unmarshals RFC3339 string to time.Time
-func UnmarshalTime(v interface{}) (time.Time, error) {
-	if tmpStr, ok := v.(string); ok {
-		return time.Parse(time.RFC3339, tmpStr)
+// UnmarshalGQL implements the graphql.Unmarshaler interface
+func (t *Time) UnmarshalGQL(v interface{}) error {
+	str, ok := v.(string)
+	if !ok {
+		return fmt.Errorf("time must be a string")
 	}
-	return time.Time{}, fmt.Errorf("time must be a string")
+
+	parsed, err := time.Parse(time.RFC3339, str)
+	if err != nil {
+		return fmt.Errorf("failed to parse time: %w", err)
+	}
+
+	*t = Time(parsed)
+	return nil
+}
+
+// ToTime converts scalar.Time to time.Time
+func (t Time) ToTime() time.Time {
+	return time.Time(t)
+}
+
+// FromTime converts time.Time to scalar.Time
+func FromTime(t time.Time) Time {
+	return Time(t)
 }
