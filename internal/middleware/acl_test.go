5f0b25a6ec050cb179e3223588f915bda59ca63b
Gushchin Andrei <neogan74@gmail.com>
2025-10-26 00:22:18 +0500

    chore: add string helper functions for test utility

--- internal/middleware/acl_test.go
+++ internal/middleware/acl_test.go
@@ -946,3 +946,17 @@ func TestGetACLCapability_NoContext(t *testing.T) {
 		t.Fatalf("request failed: %v", err)
 	}
 }
+
+// Helper function
+func contains(s, substr string) bool {
+	return len(s) >= len(substr) && (s == substr || len(s) > len(substr) && stringContains(s, substr))
+}
+
+func stringContains(s, substr string) bool {
+	for i := 0; i <= len(s)-len(substr); i++ {
+		if s[i:i+len(substr)] == substr {
+			return true
+		}
+	}
+	return false
+}
